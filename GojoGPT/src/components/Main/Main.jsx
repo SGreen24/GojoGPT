e8ccc944ec805bdd4451e7b84c517b757bda889c
SGreen24 <113150613+SGreen24@users.noreply.github.com>
2024-06-10 18:10:33 -0400

    Added Loading Animation

--- GojoGPT/src/components/Main/Main.jsx
+++ GojoGPT/src/components/Main/Main.jsx
@@ -51,10 +51,18 @@ const Main = () => {
                 </div>
                 <div className="result-data">
                     <img src={assets.gemini_icon} alt="" />
-                    <p dangerouslySetInnerHTML={{__html:resultData}}></p>
-                </div>
+
+                    {loading /* if true, its not generated */
+                    ?<div className="loader">
+                        <hr />
+                        <hr />
+                        <hr />
+                    </div>
+                    : <p dangerouslySetInnerHTML={{__html:resultData}}></p>
+                    }
             </div>
-            }
+        </div>
+        }
 
            
 
